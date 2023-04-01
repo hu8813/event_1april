dfc16a77265f5689e3fffe512ccdd7da2fcc476c
hu8813 <h0553559@gmail.com>
2023-04-01 19:42:05 +0200

    a

--- ex05/polindrome.pl
+++ ex05/polindrome.pl
@@ -2,6 +2,7 @@ print "Enter a string: ";
 $str = <STDIN>;
 if ( $str ) {
     chomp($str);
+    $str = lc($str);
     if ( $str eq reverse($str) ) {
         print "The string is a polindrome!\n";
     } else {
