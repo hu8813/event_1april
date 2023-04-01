cf017349e9559106bbeb099b2c2f9a583fb9cae4
hu8813 <h0553559@gmail.com>
2023-04-01 22:19:22 +0200

    a

--- ex11/find.py
+++ ex11/find.py
@@ -1,20 +1,21 @@
 import requests
 
-# get the name of the pokemon from the command line
-name = input("Enter the name of a pokemon: ")
-if name == "":
-    print("Error: No name given")
-    exit()
-name = name.strip().lower()
+try:
+    name = input("Enter the name of a pokemon: ")
+    if name == "":
+        raise Exception("Error: No name given")
+    name = name.strip().lower()
 
-url = f"https://pokeapi.co/api/v2/pokemon/{name}"
-response = requests.get(url)
-if response.status_code == 200:
-    data = response.json()
-    abilities = [ability['ability']['name'] for ability in data['abilities']]
-    print(f"Name: {data['name']}")
-    print("Abilities:")
-    for ability in abilities:
-        print(f"- {ability}")   
-else:
-    print("Error: Could not find Pokemon data")
+    url = f"https://pokeapi.co/api/v2/pokemon/{name}"
+    response = requests.get(url)
+    if response.status_code == 200:
+        data = response.json()
+        abilities = [ability['ability']['name'] for ability in data['abilities']]
+        print(f"Name: {data['name']}")
+        print("Abilities:")
+        for ability in abilities:
+            print(f"- {ability}")   
+    else:
+        raise Exception("Error: Could not find Pokemon data")
+except Exception as e:
+    print(f"An error occurred: {e}")
--- ex12/mail.py
+++ ex12/mail.py
@@ -9,32 +9,31 @@ subject = input("Enter email subject: ")
 body = input("Enter email message: ")
 filename = "attachment.txt"
 
-if from_addr.isempty() or to_addr.isempty() or subject.isempty() or body.isempty():
-    print("Error: Missing information")
-    exit()
+try:
+    if not from_addr or not to_addr or not subject or not body:
+        raise Exception("Error: Missing information")
 
-msg = MIMEMultipart()
-msg['From'] = from_addr
-msg['To'] = to_addr
-msg['Subject'] = subject
+    msg = MIMEMultipart()
+    msg['From'] = from_addr
+    msg['To'] = to_addr
+    msg['Subject'] = subject
 
-msg.attach(MIMEText(body))
+    msg.attach(MIMEText(body))
 
-with open(filename, 'rb') as f:
-    attachment = MIMEApplication(f.read(), _subtype='txt')
-    attachment.add_header('Content-Disposition', 'attachment', filename=filename)
-    msg.attach(attachment)
+    with open(filename, 'rb') as f:
+        attachment = MIMEApplication(f.read(), _subtype='txt')
+        attachment.add_header('Content-Disposition', 'attachment', filename=filename)
+        msg.attach(attachment)
 
-#smtp_server = 'smtp.freesmtpservers.com'
-#smtp_port = 25
+    smtp_server = 'smtp.ionos.de'
+    smtp_port = 587
+    smtp_username = ''
+    smtp_password = ''
 
-smtp_server = 'smtp.ionos.de'
-smtp_port = 587
-smtp_username = ''
-smtp_password = ''
-
-with smtplib.SMTP(smtp_server, smtp_port) as smtp:
-    smtp.starttls()
-    smtp.login(smtp_username, smtp_password)
-    smtp.sendmail(from_addr, to_addr, msg.as_string())
-    print(f'Email sent successfully to: {to_addr}')
+    with smtplib.SMTP(smtp_server, smtp_port) as smtp:
+        smtp.starttls()
+        smtp.login(smtp_username, smtp_password)
+        smtp.sendmail(from_addr, to_addr, msg.as_string())
+        print(f'Email sent successfully to: {to_addr}')
+except Exception as e:
+    print(f"An error occurred: {e}")
