2c6df86ce9beffb1b5fb4d61ce965bbff05dd7c4
hu8813 <h0553559@gmail.com>
2023-04-01 18:15:52 +0200

    a

---
+++ ex09/frame.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]) {
+    if (argc < 2) {
+        return 1;
+    }
+
+    int max_len, k = 0;
+
+    while (argv[++k])
+    {
+    max_len = 0;
+    const char delimiter[2] = " ";
+
+    char *cpy = strdup(argv[k]);
+    char *word = strtok(cpy, delimiter);
+    while (word != NULL) {
+        int len = strlen(word);
+        if (len > max_len) {
+            max_len = len;
+        }
+        word = strtok(NULL, delimiter);
+    }
+    free(cpy);
+    for (int i = 0; i < max_len + 4; i++) {
+        printf("*");
+    }
+    printf("\n");
+    strtok(NULL, delimiter);
+    cpy = strdup(argv[k]);
+        char *word2 = strtok(cpy, delimiter);
+        while (word2 != NULL) {
+            int len = strlen(word2);
+            printf("* %s", word2);
+            for (int k = 0; k < max_len - len; k++) {
+                printf(delimiter);
+            }
+            printf(" *\n");
+            word2 = strtok(NULL, delimiter);
+        }
+    free(cpy);
+    for (int i = 0; i < max_len + 4; i++) {
+        printf("*");
+    }
+    printf("\n");
+    }
+    return 0;
+}
